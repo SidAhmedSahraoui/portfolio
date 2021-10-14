035afd7aea812aa6abb6450da5745562e528abc6
sid ahmed <s.sahraoui@esi-sba.dz>
2021-10-14 09:13:14 +0100

    Updates

---
+++ precache-manifest.a8b1961e45f92eaeac849095ca9d462a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a014ae1b42f0c048e3404463d7bbc991",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "ddd837e643193a6c03ec",
+    "url": "/portofolio/static/css/main.cff8e774.chunk.css"
+  },
+  {
+    "revision": "265dc2e5c16db76578ee",
+    "url": "/portofolio/static/js/2.d7609d6f.chunk.js"
+  },
+  {
+    "revision": "189688707f34b0f0dfeb233cdc689b4b",
+    "url": "/portofolio/static/js/2.d7609d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd837e643193a6c03ec",
+    "url": "/portofolio/static/js/main.af7a14c0.chunk.js"
+  },
+  {
+    "revision": "469ca6c8f5963508014c",
+    "url": "/portofolio/static/js/runtime-main.3f30a6c4.js"
+  },
+  {
+    "revision": "d060c014216d37624e66c02a3516d429",
+    "url": "/portofolio/static/media/Home.d060c014.png"
+  },
+  {
+    "revision": "816ed47e5dbf2b77cb095d070b3fbbc2",
+    "url": "/portofolio/static/media/SOC.816ed47e.png"
+  }
+]);
